e6aa5eb5a03522e600a81ece06e627020f92994b
Nmor <nmor.moses@gmail.com>
2025-09-19 07:18:01 -0400

    Add Flask-Migrate support and initial migration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,16 @@
 import os
 import sys
+from pathlib import Path
+
 import pytest
+from flask_migrate import Migrate, downgrade, upgrade
 
 sys.path.append(os.path.abspath(os.curdir))
 from app import create_app, db
 
 
 @pytest.fixture
-def app():
+def app(tmp_path: Path):
     app = create_app()
     app.config.update(
         {
@@ -18,11 +21,15 @@ def app():
             'FLUTTERWAVE_SECRET_KEY': 'test-flutterwave',
         }
     )
+    database_path = tmp_path / "test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{database_path}"
+    Migrate(app, db)
     with app.app_context():
-        db.create_all()
+        upgrade()
         yield app
         db.session.remove()
-        db.drop_all()
+        downgrade(revision='base')
+    database_path.unlink(missing_ok=True)
 
 
 @pytest.fixture
